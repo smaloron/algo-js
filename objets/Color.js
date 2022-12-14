eb44a8db01017fcded1be18411293ce3d8e46be5
seb <contact@smaloron.eu>
2022-12-14 10:26:18 +0100

    classe Rainbow

--- objets/Color.js
+++ objets/Color.js
@@ -38,6 +38,37 @@ class Color {
     }
 }
 
+class Rainbow {
+    #colorList = [];
+
+    constructor(colorList = []) {
+        this.#colorList = colorList;
+    }
+
+    addColor (myColor) {
+        if (myColor instanceof Color) {
+            this.#colorList.push(myColor);
+        }
+        return this;
+    }
+
+    #getStripe (color) {
+        return `<div style="background-color:"#${color.getHex()}"></div>`;
+    }
+
+    show () {
+        return this.#colorList.map(item => this.#getStripe(item)).join("");
+    }
+
+    showAlt () {
+        html = "";
+        for (let c of this.#colorList) {
+            html += this.#getStripe(c);
+        }
+        return html;
+    }
+}
+
 const red = new Color(240, 59, 16);
 
 red.blue = 24;
@@ -45,3 +76,11 @@ console.log(red.getBlue());
 
 red.addBlue(14).addRed(5).addGreen(20);
 console.log(red.getHex());
+
+const rainbow = new Rainbow([new Color(16, 120, 50)]);
+rainbow
+    .addColor(new Color(90, 50, 200))
+    .addColor(new Color(37, 80, 140));
+
+console.log(rainbow.show());
+
